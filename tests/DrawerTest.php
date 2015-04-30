ba87015c8f267459496500dcd7dd07b3f943558a
Erika Heidi <erika.christina@gmail.com>
2015-04-30 14:01:39 -0300

    tests for Drawer

---
+++ tests/DrawerTest.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * DrawerTest
+ */
+
+namespace Imanee\Tests;
+
+
+use Imanee\Drawer;
+
+class DrawerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $model;
+
+    public function setup()
+    {
+        $this->model = new Drawer();
+    }
+
+    public function tearDown()
+    {
+        $this->model = null;
+    }
+
+    public function testConstructorSetsDefaults()
+    {
+        $this->assertEquals(22, $this->model->getFontSize());
+        $this->assertEquals('#000000', $this->model->getFontColor());
+    }
+
+    public function testShouldReturnDrawerObject()
+    {
+        $drawer = $this->model->getDrawer();
+
+        $this->assertInstanceOf('ImagickDraw', $drawer);
+    }
+
+    public function testShouldSetAndGetFontSize()
+    {
+        $this->model->setFontSize(20);
+
+        $this->assertEquals(20, $this->model->getFontSize());
+    }
+
+    public function testShouldSetAndGetFont()
+    {
+        $this->model->setFont('Arial.ttf');
+
+        $this->assertEquals('Arial.ttf', $this->model->getFont());
+    }
+
+    public function testShouldSetAndGetFontColor()
+    {
+        $this->model->setFontColor('Black');
+
+        $this->assertEquals('Black', $this->model->getFontColor());
+    }
+
+    public function testShouldSetAndGetTextAlign()
+    {
+        $this->model->setTextAlign(Drawer::TEXT_ALIGN_RIGHT);
+
+        $this->assertEquals(Drawer::TEXT_ALIGN_RIGHT, $this->model->getTextAlign());
+    }
+}
