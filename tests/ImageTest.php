d956105f4975150d2bb30e3d4d9e7b23b710d124
Erika Heidi <erika.christina@gmail.com>
2015-04-30 22:00:39 -0300

    starting improvement on Image Tests

--- tests/ImageTest.php
+++ tests/ImageTest.php
@@ -2,76 +2,110 @@
 
 namespace Imanee\Tests;
 
+use Imanee\Image;
+use Imanee\Imanee;
+
 class ImageTest extends \PHPUnit_Framework_TestCase
 {
     protected $test_jpg;
+    protected $model;
 
     public function setup()
     {
         $this->test_jpg = __DIR__ . '/resources/img01.jpg';
+        $this->model = new Image();
     }
 
-    public function testLoadJpg()
+    public function tearDown()
+    {
+        $this->test_jpg = null;
+        $this->model = null;
+    }
+
+    public function testShouldClone()
+    {
+        $clone = clone $this->model;
+
+        $this->assertEquals($clone, $this->model);
+    }
+
+    public function testShouldCreateNew()
+    {
+        $resource = $this->getMockBuilder('\Imagick')
+            ->disableOriginalConstructor()
+            ->setMethods(['newImage', 'getImageGeometry'])
+            ->getMock();
+
+        $resource->expects($this->once())
+            ->method('newImage')
+            ->with(100, 100);
+
+        $resource->expects($this->once())
+            ->method('getImageGeometry');
+
+        $this->model->setResource($resource);
+        $this->model->createNew(100, 100);
+    }
+
+    public function testShouldSetFormat()
+    {
+        $this->model->createNew(200, 200);
+        $this->model->setFormat('jpeg');
+
+        $this->assertEquals('jpeg', $this->model->getFormat());
+    }
+
+    public function testShouldResizeImageProportional()
+    {
+        $this->model->createNew(100, 100);
+        $this->model->setFormat('jpeg');
+
+        $this->model->resize(50, 80);
+
+        $this->assertEquals(50, $this->model->width);
+        $this->assertEquals(50, $this->model->height);
+    }
+
+    public function testShouldResizeImageExact()
+    {
+        $this->model->createNew(100, 100);
+        $this->model->setFormat('jpeg');
+
+        $this->model->resize(50, 80, false);
+
+        $this->assertEquals(50, $this->model->width);
+        $this->assertEquals(80, $this->model->height);
+    }
+
+    /**
+     * @expectedException Imanee\Exception\EmptyImageException
+     */
+    public function testShouldNotResizeIfBlank()
+    {
+        $this->model->resize(50, 80);
+    }
+
+    public function testShouldSetAndGetBackground()
+    {
+        $this->model->createNew(100, 100, 'black');
+
+        $this->assertEquals('black', $this->model->getBackground());
+    }
+
+    public function testBlankImage()
+    {
+        $this->assertTrue($this->model->isBlank());
+
+        $this->model->createNew(100, 100);
+
+        $this->assertFalse($this->model->isBlank());
+    }
+
+    /** this is not right :| */
+    public function testShouldLoadJpg()
     {
         $image = new \Imanee\Image($this->test_jpg);
 
         $this->assertSame('image/jpeg', $image->mime);
-    }
-
-    public function testCreate()
-    {
-        $image = new \Imanee\Image();
-        $image->createNew(100, 100);
-
-        $this->assertNotNull($image->getResource());
-        $this->assertSame(100, $image->width);
-    }
-
-    public function testSetFormat()
-    {
-        $image = new \Imanee\Image();
-        $image->createNew(200,200);
-        $image->setFormat('jpeg');
-
-        $this->assertSame('jpeg', $image->getFormat());
-    }
-
-    public function testBlank()
-    {
-        $image = new \Imanee\Image();
-
-        $this->assertTrue($image->isBlank());
-
-        $image->createNew(50, 50);
-
-        $this->assertFalse($image->isBlank());
-    }
-
-    public function testResize()
-    {
-        $image = new \Imanee\Image($this->test_jpg);
-        $old_w = $image->width;
-        $old_h = $image->height;
-
-        $image->resize(20,20);
-
-        $this->assertNotSame($old_w, $image->width);
-        $this->assertNotSame($old_h, $image->height);
-    }
-
-    public function testOutput()
-    {
-        $image = new \Imanee\Image($this->test_jpg);
-        $output = $image->output();
-
-        $this->assertNotNull($output);
-    }
-
-    public function testSetBackground()
-    {
-        $image = new \Imanee\Image();
-        $image->createNew(100, 100, 'black');
-
-        $this->assertSame('black', $image->getBackground());
     }
 }
